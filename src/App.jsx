6fb46540da45d206a4b536aa582d8c1c5b054967
akash-nagar-95 <anagar@trueigtech.com>
2025-02-11 14:17:44 +0530

    Task-3 : Created a Counter App

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@
 // import Footer from './components/Footer';
 // import Heading from './components/Heading';
 // import MainContent from './components/MainContent';
-
 import Counter_App from "./components/Counter_App";
+// import Api from "./components/Api";
 
 
 const App = () => {
   return (
     <>
-      {/* <Heading /> */}
+      {/* <Heading />  */}
       {/* <MainContent  /> */}
       {/* <Footer /> */}
       <Counter_App />
+      {/* <Api /> */}
+
+
+      
 
     </>
   )
