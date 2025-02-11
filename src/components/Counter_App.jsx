44028e4e3d55284e33e3da76002e66af6018344c
akash-nagar-95 <anagar@trueigtech.com>
2025-02-11 14:17:44 +0530

    Task: Task-3 Completed

--- src/components/Counter_App.jsx
+++ src/components/Counter_App.jsx
@@ -3,17 +3,15 @@ import { useState } from 'react'
 const Counter_App = () => {
     const [counter, setCounter] = useState(0);
     const handleIncrement = () => setCounter(counter + 1);
-
     const handleDecrement = () => setCounter(counter - 1);
 
   return (
-    <div>
+    <div style={{marginLeft:"500px", marginTop:"300px" }}>
       <p>
         To increment the value press here : 
         <button style={{backgroundColor: "green", color: "black", borderRadius: "2px solid black"}} onClick={handleIncrement}>Increment</button> 
         <span style={{marginLeft: "20px", fontSize: "20px"}}>{counter}</span>
       </p>
-      <hr />
       <p>
         To decrement the value press here : 
         <button style={{backgroundColor: "red", color: "black", borderRadius: "2px solid black"}} onClick={handleDecrement}>Decrement</button> 
