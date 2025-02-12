2808ea503e167760bf04567f6ccf52c7d29a70bf
akash-nagar-95 <anagar@trueigtech.com>
2025-02-13 01:02:02 +0530

    Task-5 : Controlled Form created

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@
 // import Footer from './components/Footer';
 // import Heading from './components/Heading';
 // import MainContent from './components/MainContent';
-import Counter_App from "./components/Counter_App";
+// import Counter_App from "./components/Counter_App";
 // import Api from "./components/Api";
+
+import ControlledForm from "./components/ControlledForm";
 
 
 const App = () => {
@@ -12,8 +14,14 @@ const App = () => {
       {/* <Heading />  */}
       {/* <MainContent  /> */}
       {/* <Footer /> */}
+<<<<<<< Updated upstream
       <Counter_App />
       {/* <Api /> */}
+=======
+      {/* <Counter_App /> */}
+      {/* <Api /> */}
+      < ControlledForm />
+>>>>>>> Stashed changes
 
 
       
