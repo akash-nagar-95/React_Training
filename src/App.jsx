ba5add46e0e046b448d3fd1daa183056715931f9
akash-nagar-95 <anagar@trueigtech.com>
2025-02-13 10:24:09 +0530

    ControlledForm created

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,31 @@
 // import Heading from './components/Heading';
 // import MainContent from './components/MainContent';
 // import Counter_App from "./components/Counter_App";
-import Api from "./components/Api";
+import { BrowserRouter, Link } from "react-router-dom";
+// import Api from "./components/Api";
+import Routing from "./components/Routing";
 
 
 const App = () => {
   return (
-    <>
-      {/* <Heading />  */}
-      {/* <MainContent  /> */}
-      {/* <Footer /> */}
-      {/* <Counter_App /> */}
-      <Api />
+    
+      // {/* <Heading />  */}
+      // {/* <MainContent  /> */}
+      // {/* <Footer /> */}
+      // {/* <Counter_App /> */}
+      // {/* <Api /> */}
+      <BrowserRouter>
+        <nav>
+          <Link to='/' >  Home  </Link>
+          <Link to='/about'>  About  </Link>
+          <Link to='/contact'>  Contacts  </Link>
+        </nav>
+        <Routing />
+      </BrowserRouter>
 
 
-      
-
-    </>
-  )
+  
+  );
 }
 
 export default App;
